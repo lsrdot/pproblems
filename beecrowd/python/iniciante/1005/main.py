1d5cd7b8697ca2fd690544b78b218e66cb870d3a
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2022-11-02 20:50:56 -0300

    add problem1005

--- beecrowd/python/iniciante/1005/main.py
+++ beecrowd/python/iniciante/1005/main.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
+PESO = (3.5, 7.5)
+
+
 def main():
-    ...
+    a, b = input(), input()
+    if isfloat(a, b):
+        a, b = float(a), float(b)
+        media = (a * PESO[0] + b * PESO[1]) / (PESO[0] + PESO[1])
+        print(f'MEDIA = {media:.5f}')
+
+
+def isfloat(*n):
+    try:
+        for x in n:
+            float(x)
+        return True
+    except ValueError:
+        return False
 
 
 if __name__ == "__main__":
